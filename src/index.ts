531e49dc021fc430b1011b61615df01a198575e8
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2025-07-02 16:05:44 -0300

    chore(create post): create type and post request

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,38 @@
-import express from "express";
+import express, {Request, Response} from "express";
 
 const app = express();
+app.use(express.json());
 
-app.listen(3000,() => {
+let userRegired: number = 0;
+let id: number = 1;
+
+type User = {id: number, name: string, email: string};
+let users: User[] = []
+
+app.listen(3000, () => {
     console.log("Servidor online na porta 3000");
 });
 
-app.get("/",(req,res) => {
+app.get("/users", (req: Request, res: Response) => {
+    res.send(users)
+});
+
+app.get("/users/:id", (req: Request, res: Response) => {
+   let userId: number = Number(req.params.id);
+   let user = users.find(user => user.id === userId);
+   res.send(user)
+});
+
+app.post("/users", (req: Request, res: Response) => {
+    let user = req.body;
+    user.id = id++;
+    userRegired++;
+    users.push(user);
+    res.send({
+        message: `Usuario criado com sucesso ${userRegired}`
+    });
+});
+
+app.get("/", (req: Request, res: Response) => {
     res.send("Mundao e loko ")
 });
