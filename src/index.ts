ef4747a1522700b6c7c4370e182201f48c2f3e89
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2025-07-02 16:53:05 -0300

    chore(create put): create put method

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,18 @@ import express, {Request, Response} from "express";
 const app = express();
 app.use(express.json());
 
-let userRegired: number = 0;
+let userRegistered: number = 0;
 let id: number = 1;
 
-type User = {id: number, name: string, email: string};
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
 let users: User[] = []
-
+/**
+ * Iniciando o servidor
+ * */
 app.listen(3000, () => {
     console.log("Servidor online na porta 3000");
 });
@@ -18,18 +24,30 @@ app.get("/users", (req: Request, res: Response) => {
 });
 
 app.get("/users/:id", (req: Request, res: Response) => {
-   let userId: number = Number(req.params.id);
-   let user = users.find(user => user.id === userId);
-   res.send(user)
+    let userId: number = Number(req.params.id);
+    let user = users.find(user => user.id === userId);
+    res.send(user)
 });
+
+app.put("/users/:id", (req: Request, res: Response) => {
+    let userId: number = Number(req.params.id);
+    let user = req.body;
+    let indexOfUser: number = users.findIndex((_user: User) => _user.id === userId);
+    users[indexOfUser].name = user.name;
+    users[indexOfUser].email = user.email;
+    res.send({
+        message: `Antigo usuario -> ${users[indexOfUser].name} alterado para -> 
+        ${user.name} email atualizado de ${users[indexOfUser].email} para -> ${user.email}`
+    });
+})
 
 app.post("/users", (req: Request, res: Response) => {
     let user = req.body;
     user.id = id++;
-    userRegired++;
+    userRegistered++;
     users.push(user);
     res.send({
-        message: `Usuario criado com sucesso ${userRegired}`
+        message: `Usuario criado com sucesso ${userRegistered}`
     });
 });
 
