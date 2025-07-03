07ec03645be0369f4d8f1c2e8052eac490e71fba
Gabriel Alves <gcarmodev@gdsdesenvolvimento.com>
2025-07-03 12:44:31 -0300

    feat(Controller): create first controller

---
+++ src/controllers/users.controller.ts
@@ -0,0 +1,53 @@
+import express, {Request, Response} from 'express';
+let userRegistered: number = 0;
+let id: number = 1;
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
+let users: User[] = []
+
+export class UsersController {
+    static getAll(req: Request, res: Response): void{
+        res.send(users)
+    }
+
+    static getById(req: Request, res: Response): void{
+        let userId: number = Number(req.params.id);
+        let user = users.find(user => user.id === userId);
+        res.send(user)
+    }
+
+    static change(req: Request, res: Response): void{
+        let userId: number = Number(req.params.id);
+        let user = req.body;
+        let indexOfUser: number = users.findIndex((_user: User): boolean => _user.id === userId);
+        users[indexOfUser].name = user.name;
+        users[indexOfUser].email = user.email;
+        res.send({
+            message: `Antigo usuario -> ${users[indexOfUser].name} alterado para -> 
+        ${user.name} email atualizado de ${users[indexOfUser].email} para -> ${user.email}`
+        });
+    }
+
+    static create(req: Request, res: Response): void{
+        let user = req.body;
+        user.id = id++;
+        userRegistered++;
+        users.push(user);
+        res.send({
+            message: `Usuario criado com sucesso ${userRegistered}`
+        });
+    }
+
+    static delete(req: Request, res: Response): void{
+        let userId: number = Number(req.params.id);
+        let indexOfUser: number = users.findIndex((_user: User): boolean => _user.id === userId);
+        users.splice(indexOfUser, 1);
+        res.send({
+            message: `Usuario removido com sucesso`
+        });
+    }
+}
--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,60 +1,15 @@
-import express, {Router, Request, Response} from "express";
+import express, {Router} from "express";
+import {UsersController} from "../controllers/users.controller";
 
 export const userRouter: Router = express.Router();
 
-let userRegistered: number = 0;
-let id: number = 1;
-
-type User = {
-    id: number;
-    name: string;
-    email: string;
-};
-let users: User[] = []
-
-
 // Get all Users
-userRouter.get("/users", (req: Request, res: Response): void => {
-    res.send(users)
-});
-
+userRouter.get("/users", UsersController.getAll);
 //Get user for id
-userRouter.get("/users/:id", (req: Request, res: Response): void => {
-    let userId: number = Number(req.params.id);
-    let user = users.find(user => user.id === userId);
-    res.send(user)
-});
-
+userRouter.get("/users/:id", UsersController.getById);
 //Put change data user
-userRouter.put("/users/:id", (req: Request, res: Response): void => {
-    let userId: number = Number(req.params.id);
-    let user = req.body;
-    let indexOfUser: number = users.findIndex((_user: User):boolean => _user.id === userId);
-    users[indexOfUser].name = user.name;
-    users[indexOfUser].email = user.email;
-    res.send({
-        message: `Antigo usuario -> ${users[indexOfUser].name} alterado para -> 
-        ${user.name} email atualizado de ${users[indexOfUser].email} para -> ${user.email}`
-    });
-});
-
+userRouter.put("/users/:id",UsersController.change);
 // Post send user
-userRouter.post("/users", (req: Request, res: Response): void => {
-    let user = req.body;
-    user.id = id++;
-    userRegistered++;
-    users.push(user);
-    res.send({
-        message: `Usuario criado com sucesso ${userRegistered}`
-    });
-});
-
+userRouter.post("/users", UsersController.create);
 //Delete user
-userRouter.delete("/users/:id", (req: Request, res: Response): void => {
-    let userId: number = Number(req.params.id);
-    let indexOfUser: number = users.findIndex((_user: User): boolean => _user.id === userId);
-    users.splice(indexOfUser, 1);
-    res.send({
-        message: `Usuario removido com sucesso`
-    });
-})
+userRouter.delete("/users/:id",UsersController.delete)
